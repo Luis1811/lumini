872595c6d1f99fa3b9c4ce62193d4131758e4742
Glya-Corporation <alfonsouzcategui2@gmail.com>
2023-02-23 12:18:10 -0500

    second commit1

--- src/main/java/com/C9group34/socialnetworkproject/controllers/UserController.java
+++ src/main/java/com/C9group34/socialnetworkproject/controllers/UserController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "{host}")
 public class UserController {
     /*public ResponseEntity<User> getUser(){
         return null;
