d970160d6fcc9cd3fab85da463d7381b26995b80
Glya-Corporation <alfonsouzcategui2@gmail.com>
2023-02-24 14:46:48 -0500

    CORS 5 global

--- src/main/java/com/C9group34/socialnetworkproject/controllers/UserController.java
+++ src/main/java/com/C9group34/socialnetworkproject/controllers/UserController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
 
